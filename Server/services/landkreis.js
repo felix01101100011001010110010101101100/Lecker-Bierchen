82c10fb4194064c5727c3d7c41a1af0aa62e5ee9
pichen2 <pia.bauer.1@gmx.de>
2024-05-31 13:32:55 +0200

    Landkreisid rausholen

--- Server/services/landkreis.js
+++ Server/services/landkreis.js
@@ -1,5 +1,18 @@
 const express = require('express');
 var router = express.Router();
 
+class LandkreisDao{
+    constructor(dbConnection){
+        this.dbconnection = dbConnection;
+    }
+
+    landkreisAufrufen(landkreisname){
+        var ret = this.dbconnection.get("SELECT id FROM Lankreis WHERE name=?"[landkreisname]);
+        return ret;
+    }
+
+
+}
+
 
 module.exports = router;
