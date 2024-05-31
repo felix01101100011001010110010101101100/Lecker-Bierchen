e7b1aacf941ebc06259c8f3c5e1927719efa15d0
pichen2 <pia.bauer.1@gmx.de>
2024-05-31 10:47:24 +0200

    Änderungen an der Struktur

---
+++ public/scripts/navigation.js
@@ -0,0 +1,36 @@
+//const { json } = require("express");
+
+function get_home_html(){
+    $.ajax({
+        url: '/home.html',
+        type: 'GET',
+        beforeSend: setAuthentification,
+        success: function(data) {
+            console.log('Klappt:');
+            $('body').html(data);
+        },
+        error: function( errorThrown) {
+            console.log('Error:', errorThrown);
+        }
+    });       
+
+}
+
+
+function get_profil_html(){
+    $.ajax({    
+        url: '/profil.html',
+        type: 'GET',
+        beforeSend: setAuthentification,
+        success: function(data) {
+            console.log('Klappt');
+            $('body').html(data);
+        },
+        error: function(errorThrown) {
+            console.log('Error:', errorThrown);
+        }
+    });       	
+    
+
+}
+
---
+++ public/scripts/profil.js
@@ -0,0 +1,26 @@
+//Funktion, um die Daten im Profil anzugucken
+function dynamischProfil(){
+    $.ajax({
+        url: "/api/profil",
+        type: "GET",
+        beforeSend: setAuthentification,
+        success: function(data){
+            $("#vnProfil").val(data.vorname)
+            $("#nnProfil").val(data.nachname)
+            $("#age").val(data.jahr)
+            $("#bnProfil").val(data.benutzername)
+            $("#lkProfil").val(data.name)
+        
+
+            if(data.fuehrerschein == 1){
+                $("#dl").val("Ja")
+            }
+            else{
+                $("#dl").val("Nein")
+            }
+        },
+        error: function(error){
+            console.error("Error: ", error)
+        }
+    })
+}                   
