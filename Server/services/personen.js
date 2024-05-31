bf15b376082d13c4e7e7586580fe49b1c3fe8e6b
pichen2 <pia.bauer.1@gmx.de>
2024-05-31 12:59:08 +0200

    Änderung von api/ weg

--- Server/services/personen.js
+++ Server/services/personen.js
@@ -131,7 +131,7 @@ router.post('/login', (req, res) => {
 
 
 //Funktion (oder was auch immer), um die Daten vom Server fürs Profil an Client zu senden
-router.get("/api/profil", verifyToken, (req,res)=>{
+router.get("/profil", verifyToken, (req,res)=>{
     var datenDieZurueckGehen = personDao.personenDatenAbrufen(request.app.locals.dbConnection);
     res.json(datenDieZurueckGehen);
 
