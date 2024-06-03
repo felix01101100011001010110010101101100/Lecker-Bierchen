ad15afb74b8df93776b8b52db19f5dd1b5945b99
pichen2 <pia.bauer.1@gmx.de>
2024-06-03 11:02:05 +0200

    gruppen dynamisch gearbeitet

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -1,11 +1,16 @@
 function dynamischEventInGruppe(){
+    var inhalt = ""
     $.ajax({
         url:"/gruppen/event",
         type:"GET",
         beforeSend: setAuthentification,
         success: function(data){
-            
+            inhalt = "<section><i class='fa-solid fa-car fa-5x' id='auto'></i><p id='eventname'><b>" + Name + "</b> <b>" + Ort +"</b> <b>"+Uhrzeit+"</b> </p>"+
+            "<p id='beschreibung'>Beschreibung: "+ Beschreibung+ "</p><p id='fahrername'> "+Mustermann+"</p>"+
+            "<p><button type='submit' class='erstellen' id='dabei'>Bin dabei!</button> <button type='submit' class='erstellen' id='raus5'>Bin raus!</button></p> </section>"
 
+
+            
         }
 
     })
