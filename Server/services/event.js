b6b5b3d4fba1214b174ff8cb079c262238e2fd98
pichen2 <pia.bauer.1@gmx.de>
2024-06-03 14:57:16 +0200

    Fahrersuche weitergemacht

--- Server/services/event.js
+++ Server/services/event.js
@@ -5,10 +5,14 @@ var router = express.Router();
 router.get("/eventUebersicht", verifyToken, (req, res)=>{
     const eventDao = new EventDao(request.app.locals.dbConnection);
 
+    anzahlMenschen = eventDao.anzahlMenschenImEvent(eventid);
+
     datenDieZurueckGehen = eventDao.loadById(id);
+    
+
     res.send(datenDieZurueckGehen);
 
-    
+
 })
 
 
