0ece113db5b3b6c0553d81b27225e937cb69d864
pichen2 <pia.bauer.1@gmx.de>
2024-06-03 13:13:42 +0200

    irgendwelche Änderungen

--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -8,6 +8,7 @@ function dynamischEventUebersicht(){
             var inhalt = "<section id='uebersichtSection'><p id='eventnameUebersicht'><b>"+ data.eventname + "</b> <b> "+ data.ort +"</b> <b>"+ data.zeit 
             +"</b> <b>Gruppe: "+ data.gruppenname + "</b> <b>Fahrer: " + Name +" </b></p><p id='beschreibung'>Beschreibung: "+ 
             data.bemerkung + "</p> </section>"
+            return inhalt;
 
 
 
