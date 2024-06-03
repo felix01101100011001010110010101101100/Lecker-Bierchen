950b89d1e61f127985cefafa198e6a962fd4cbe8
pichen2 <pia.bauer.1@gmx.de>
2024-06-03 10:36:56 +0200

    dynamisch Event Übersich ajax Call gemacht

--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -5,9 +5,9 @@ function dynamischEventUebersicht(){
         type: "GET",
         beforeSend: setAuthentification, //könnte nicht funktionieren, da die Funktion vlt. nicht erreichbar ist
         success: function(data){
-            var inhalt = "<section id='uebersichtSection'><p id='eventnameUebersicht'><b>"+ Name + "</b> <b> "+ Ort +"</b> <b>"+ Uhrzeit 
-            +"</b> <b>Gruppe: "+ Gruppenname + "</b> <b>Fahrer: " + Name +" </b></p><p id='beschreibung'>Beschreibung: "+ 
-            Text + "</p> </section>"
+            var inhalt = "<section id='uebersichtSection'><p id='eventnameUebersicht'><b>"+ data.eventname + "</b> <b> "+ data.ort +"</b> <b>"+ data.zeit 
+            +"</b> <b>Gruppe: "+ data.gruppenname + "</b> <b>Fahrer: " + Name +" </b></p><p id='beschreibung'>Beschreibung: "+ 
+            data.bemerkung + "</p> </section>"
 
 
 
