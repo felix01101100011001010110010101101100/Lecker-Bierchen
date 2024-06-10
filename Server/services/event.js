5eb17d18f432da19ad055700466faae4173af99a
pichen2 <pia.bauer.1@gmx.de>
2024-06-10 10:23:06 +0200

    event übersicht weiter gemacht

--- Server/services/event.js
+++ Server/services/event.js
@@ -8,10 +8,10 @@ router.get("/eventUebersicht", verifyToken, (req, res)=>{
 
     //anzahlMenschen = eventDao.anzahlMenschenImEvent(eventid);
 
-    datenDieZurueckGehen = eventDao.loadById(id);
-    res.send(datenDieZurueckGehen);
+    //datenDieZurueckGehen = eventDao.loadById(id);
+    //res.send(datenDieZurueckGehen);
     //res.send(anzahlMenschen);
-    //res.json({eventname:"Die coolen Hosen", ort: "Albstadt", zeit: "15 Uhr", gruppenname: "Nice", bemerkung: "Alle bringen ihre eigenen Getränke mit."})
+    res.json({eventname:"Die coolen Hosen", ort: "Albstadt", zeit: "15 Uhr", gruppenname: "Nice", bemerkung: "Alle bringen ihre eigenen Getränke mit."})
 
 })
 
