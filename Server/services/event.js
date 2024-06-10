c4dcd55ec41acc89a7561e7f9fd7f68858e9ff77
pichen2 <pia.bauer.1@gmx.de>
2024-06-10 10:22:18 +0200

    event übersicht weiter gemacht

--- Server/services/event.js
+++ Server/services/event.js
@@ -4,14 +4,14 @@ const EventDao = require('../dao/eventDao.js');
 var router = express.Router();
 
 router.get("/eventUebersicht", verifyToken, (req, res)=>{
-    const eventDao = new EventDao(request.app.locals.dbConnection);
+    const eventDao = new EventDao(req.app.locals.dbConnection);
 
     //anzahlMenschen = eventDao.anzahlMenschenImEvent(eventid);
 
     datenDieZurueckGehen = eventDao.loadById(id);
     res.send(datenDieZurueckGehen);
     //res.send(anzahlMenschen);
-    res.json({eventname:"Die coolen Hosen", ort: "Albstadt", zeit: "15 Uhr", gruppenname: "Nice", Name: "Hans", bemerkung: "Alle bringen ihre eigenen Getränke mit."})
+    //res.json({eventname:"Die coolen Hosen", ort: "Albstadt", zeit: "15 Uhr", gruppenname: "Nice", bemerkung: "Alle bringen ihre eigenen Getränke mit."})
 
 })
 
--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -9,7 +9,7 @@ function dynamischEventUebersicht(){
             console.log(data);
 
             var inhalt = "<section id='uebersichtSection'><p id='eventnameUebersicht'><b>"+ data.eventname + "</b> <b> "+ data.ort +"</b> <b>"+ data.zeit 
-            +"</b> <b>Gruppe: "+ data.gruppenname + "</b> <b>Fahrer: " + Name +" </b></p><p id='beschreibung'>Beschreibung: "+ 
+            +"</b> <b>Gruppe: "+ data.gruppenname + "</b> <b>Fahrer: </b></p><p id='beschreibung'>Beschreibung: "+ 
             data.bemerkung + "</p> </section>"
 
             $("#uebersichtMain").html(inhalt);
