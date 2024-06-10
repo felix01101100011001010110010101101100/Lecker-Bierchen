6a260ebfbb74cb0f347f8368802e20d998c2605e
pichen2 <pia.bauer.1@gmx.de>
2024-06-10 11:34:27 +0200

    event erstellen weiter gemacht

--- public/scripts/event_erstellen.js
+++ public/scripts/event_erstellen.js
@@ -3,13 +3,14 @@ function eventErstellen(){
     var ort = $("#ort").val();
     var zeit = $("#zeit").val();
     var bemerkung = $("#bemerkung").val();
-    
+
     $.ajax({
         url:"/event/in/gruppe/erstellen",
         type:"POST",
         beforeSend: setAuthentification,
+        data: {eventname: eventname, ort: ort, zeit: zeit, bemerkung: bemerkung},
         success: function(data){
-            
+            console.log("funktioniert");
         },
         error: function(error){
             console.error("Error: ", error) //vlt. alert
