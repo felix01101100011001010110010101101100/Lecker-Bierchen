4cb2624744710d962d0b3e1970cbbc3ec496851a
pichen2 <pia.bauer.1@gmx.de>
2024-06-17 14:43:37 +0200

    änderungen

--- Server/services/gruppe.js
+++ Server/services/gruppe.js
@@ -10,8 +10,15 @@ router.get("/gruppe/home/anzeigen", verifyToken, (req, res)=>{
     let daten2 = {
         gruppenname: "Die coolen Hosen"
     };
+    
+    let daten3 = {
+        gruppenname: "Die coolen Hosen"
+    }
+    let daten4 = {
+        gruppenname: "Die coolen Hosen"
+    }
 
-    res.json([daten1, daten2]);
+    res.json([daten1, daten2,daten3, daten4]);
 })
 
 module.exports = router;
--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -8,10 +8,13 @@ function dynamischEventUebersicht(){
             //hier muss die Berechnung der Fahrersuche hin
             console.log(data);
 
-
-            var inhalt = "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b> " + data.eventname + "</td> <td> <b>Ort:</b> " + data.ort + "</td> <td> <b>Zeit:</b> " + data.zeit 
-            + "</td><td> <b>Gruppe:</b> "+ data.gruppenname + "</td><td> <b>Fahrer:</b> </td> <tr><td colspan='4' id='beschreibung'>Beschreibung: "+ 
-            data.bemerkung + "</td></tr></table> </section>"
+            var inhalt = ""
+            data.forEach(function(){
+                inhalt += "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b> " + data.eventname + "</td> <td> <b>Ort:</b> " + data.ort + "</td> <td> <b>Zeit:</b> " + data.zeit 
+                + "</td><td> <b>Gruppe:</b> "+ data.gruppenname + "</td><td> <b>Fahrer:</b> </td> <tr><td colspan='4' id='beschreibung'>Beschreibung: "+ 
+                data.bemerkung + "</td></tr></table> </section>"
+            })
+            
 
             $("#uebersichtMain").html(inhalt);
 
