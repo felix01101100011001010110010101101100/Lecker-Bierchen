3469ea3bc864f84c0551992258a4bfde81d2dcbf
Kajuk5603 <kajuk5603@gmail.com>
2024-06-17 13:35:20 +0200

    eventübersicht

--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -8,8 +8,8 @@ function dynamischEventUebersicht(){
             //hier muss die Berechnung der Fahrersuche hin
             console.log(data);
 
-            var inhalt = "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td>Eventname: "+ data.eventname + "</td> <td>Ort: "+ data.ort +"</td> <td>Zeit: "+ data.zeit 
-            +"</td><td> Gruppe: "+ data.gruppenname + "</td><td> Fahrer:</td> <tr><td colspan='4' id='beschreibung'>Beschreibung: "+ 
+            var inhalt = "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b> " + data.eventname + "</td> <td> <b>Ort:</b> " + data.ort + "</td> <td> <b>Zeit:</b> " + data.zeit 
+            + "</td><td> <b>Gruppe:</b> "+ data.gruppenname + "</td><td> <b>Fahrer:</b> </td> <tr><td colspan='4' id='beschreibung'>Beschreibung: "+ 
             data.bemerkung + "</td></tr></table> </section>"
 
             $("#uebersichtMain").html(inhalt);
