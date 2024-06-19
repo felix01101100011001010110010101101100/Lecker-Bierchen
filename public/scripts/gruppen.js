53ab72f1ed6ad06ce691ca81e76134a13b5a2875
FelixSauter <felixsauter100@gmail.com>
2024-06-19 13:34:07 +0200

    diverses

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -1,13 +1,12 @@
-function dynamischEventInGruppe(){
+function dynamischEventInGruppe(gruppenid){
     var inhalt = ""
 
     // id aus dem Session Storage abrufen. Er wird bei der verification mit abgespeichert
-    id = sessionStorage.getItem('id');
     $.ajax({
         url:"/gruppen/event",
         type:"GET",
         beforeSend: setAuthentification,
-        data: {id: id},
+        data: {gruppenid: gruppenid},
         success: function(data){
             inhalt = "<section><i class='fa-solid fa-car fa-5x' id='auto'></i><p id='eventname'><b>" + Name + "</b> <b>" + Ort +"</b> <b>"+Uhrzeit+"</b> </p>"+
             "<p id='beschreibung'>Beschreibung: "+ Beschreibung+ "</p><p id='fahrername'> "+Mustermann+"</p>"+
