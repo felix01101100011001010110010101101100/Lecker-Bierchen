cc199e5f52e5a2d391d81518371df5b8f97f3f26
Kajuk5603 <kajuk5603@gmail.com>
2024-06-24 14:37:29 +0200

    dabei/raus Funktionen + entfern button in event übersicht

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -1,4 +1,4 @@
-function gruppenName(){
+function gruppenName(){  //path fehlt noch -> Backend 
     var inhalt = ""
     $.ajay({
         url:"/gruppen/name",
@@ -22,7 +22,7 @@ function dynamischEventInGruppe(gruppenid){
         success: function(data){
             inhalt = "<section><i class='fa-solid fa-car fa-5x' id='auto'></i><p id='eventname'><b>" + data.eventname + "</b> <b>" + data.ort +"</b> <b>"+data.zeit+"</b> </p>"+
             "<p id='beschreibung'>Beschreibung: "+ data.bemerkung+ "</p><p id='fahrername'> </p>"
-            "<p><button type='submit' class='erstellen' id='dabei'>Bin dabei!</button> <button type='submit' class='erstellen' id='raus5'>Bin raus!</button></p> </section>"  
+            "<p><button type='submit' class='erstellen' id='dabei' onclick='eventDabei()'>Bin dabei!</button> <button type='submit' class='erstellen' id='raus' onclick='eventRaus()'>Bin raus!</button></p> </section>"  
 
             $("main").hmtl(inhalt);
         }
