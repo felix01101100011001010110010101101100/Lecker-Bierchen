5f0f0faaa16a6d5b8696e3f959b0ae1650a51af1
Kajuk5603 <kajuk5603@gmail.com>
2024-06-24 11:57:37 +0200

    änderungen

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -1,6 +1,6 @@
 function dynamischEventInGruppe(gruppenid){
     var inhalt = ""
-
+    
     // id aus dem Session Storage abrufen. Er wird bei der verification mit abgespeichert
     $.ajax({
         url:"/gruppen/event",
@@ -10,8 +10,9 @@ function dynamischEventInGruppe(gruppenid){
         success: function(data){
             inhalt = "<section><i class='fa-solid fa-car fa-5x' id='auto'></i><p id='eventname'><b>" + data.eventname + "</b> <b>" + data.ort +"</b> <b>"+data.zeit+"</b> </p>"+
             "<p id='beschreibung'>Beschreibung: "+ data.bemerkung+ "</p><p id='fahrername'> </p>"+
-            "<p><button type='submit' class='erstellen' id='dabei'>Bin dabei!</button> <button type='submit' class='erstellen' id='raus5'>Bin raus!</button></p> </section>"
-            
+            "<p><button type='submit' class='erstellen' id='dabei'>Bin dabei!</button> <button type='submit' class='erstellen' id='raus5'>Bin raus!</button></p> </section>"  
+
+            $("main").hmtl(inhalt);
         }
 
     })
