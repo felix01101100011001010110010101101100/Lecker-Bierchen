9e3502e16c583f8729572b7c396240fa646ffe44
Kajuk5603 <kajuk5603@gmail.com>
2024-06-24 15:18:44 +0200

    test

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -23,7 +23,7 @@ function dynamischEventInGruppe(gruppenid){
             inhalt = "<section><i class='fa-solid fa-car fa-5x' id='auto'></i><p id='eventname'><b>" + data.eventname + "</b> <b>" + data.ort +"</b> <b>"+data.zeit+"</b> </p>"+
             "<p id='beschreibung'>Beschreibung: "+ data.bemerkung+ "</p><p id='fahrername'> </p>"
             "<p><button type='submit' class='erstellen' id='dabei' value='1' onclick='eventDabei()'>Bin dabei!</button></p> </section>"  
-
+            console.log(data)
             $("main").hmtl(inhalt);
         }
 
