8e007e3548b5bd85e8dd72a76e68fb8d21c0d17c
Kajuk5603 <kajuk5603@gmail.com>
2024-06-24 14:07:52 +0200

    gruppennamen dynamisch

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -1,6 +1,18 @@
+function gruppenName(){
+    var inhalt = ""
+    $.ajay({
+        url:"/gruppen/name",
+        type:"GET",
+        beforeSend: setAuthentification,
+        success: function(data){
+            inhalt= data.gruppennname
+            $("body").html(inhalt);
+        }
+    })
+}
+
 function dynamischEventInGruppe(gruppenid){
     var inhalt = ""
-    
     // id aus dem Session Storage abrufen. Er wird bei der verification mit abgespeichert
     $.ajax({
         url:"/gruppen/event",
@@ -9,14 +21,13 @@ function dynamischEventInGruppe(gruppenid){
         data: {gruppenid: gruppenid},
         success: function(data){
             inhalt = "<section><i class='fa-solid fa-car fa-5x' id='auto'></i><p id='eventname'><b>" + data.eventname + "</b> <b>" + data.ort +"</b> <b>"+data.zeit+"</b> </p>"+
-            "<p id='beschreibung'>Beschreibung: "+ data.bemerkung+ "</p><p id='fahrername'> </p>"+
+            "<p id='beschreibung'>Beschreibung: "+ data.bemerkung+ "</p><p id='fahrername'> </p>"
             "<p><button type='submit' class='erstellen' id='dabei'>Bin dabei!</button> <button type='submit' class='erstellen' id='raus5'>Bin raus!</button></p> </section>"  
 
             $("main").hmtl(inhalt);
         }
 
     })
-
 }
 
 function eventErstellenButton(){
