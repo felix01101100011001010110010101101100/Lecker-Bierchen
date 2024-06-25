402ba4385c0253d920b05fc8afa752c6c96e08cb
pichen2 <pia.bauer.1@gmx.de>
2024-06-25 16:10:54 +0200

    key gruppe beitreten

--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -17,10 +17,7 @@ function dynamischEventUebersicht(){
                 event.bemerkung + "</td></tr></table> </section>";
             });
             $("#uebersichtMain").html(inhalt);
-             /*   
-            inhalt += "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b>"+ event.eventname+" </td> <td> <b>Ort:</b> " + event.ort + "</td> <td> <b>Zeit:</b> " + event.zeit 
-                + "</td><td> <b>Gruppe:</b> "+ event.gruppenname + "</td><td> <b>Fahrer:</b> </td><td></td><td></td><td></td><td></td><td><td></td><td></td><td></td><td></td><td></td><td></td><td><i id='eventEntfernen' class='fa-solid fa-xmark'></td> <tr><td colspan='10' id='beschreibung'>Beschreibung: "+ 
-                event.bemerkung + "</td></tr></table> </section>";*/
+            
             //Fahrersuche: alle Namen in der Datenbank ausgegeben, die an dem Event teilnehmen (count(*) benutzen, aber wir ruft man 
             //die auf???)und dann in eine Liste packen und dann
             //einen Zufallszahlengenerator anwenden und die Person, die an dieser Stelle steht muss fahren
