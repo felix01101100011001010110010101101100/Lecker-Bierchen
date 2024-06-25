8c16fb882e54cfc85b72fb75610062c5066dd7e0
Kajuk5603 <kajuk5603@gmail.com>
2024-06-25 17:08:14 +0200

    nach dem Löschen zur Home-Page

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -72,7 +72,20 @@ function gruppeVerlassen(){
             console.error("Error: ", error) 
             alert("Sie konnten die Gruppe nicht verlassen")
         },
+        
     })
+    .then($.ajax({
+        url: "/home.html",
+        type: "GET",
+        beforeSend: setAuthentification,
+        success: function(data){
+            $("body").html(data)
+        },
+        error: function(error){
+            console.error("Error ", error);
+            alert("Seite konnte nicht geladen werden");
+        },
+    }))
 }
 
 function mitgliederAnzeigen(){
