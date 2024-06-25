fcdef5883ef47e9a5adacb0ea30de7f0eb98da26
pichen2 <pia.bauer.1@gmx.de>
2024-06-25 15:54:58 +0200

    änderungen

--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -13,7 +13,7 @@ function dynamischEventUebersicht(){
             
             data.forEach(function(event){
                 inhalt += "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b> ${event.eventname} </td> <td> <b>Ort:</b> " + event.ort + "</td> <td> <b>Zeit:</b> " + event.zeit 
-                + "</td><td> <b>Gruppe:</b> "+ event.gruppenname + "</td><td> <b>Fahrer:</b> </td><td></td><td></td><td></td><td></td><td><td></td><td></td><td></td><td></td><td></td><td></td><td><i id='eventEntfernen' class='fa-solid fa-xmark'></td> <tr><td colspan='10' id='beschreibung'>Beschreibung: "+ 
+                + "</td><td> <b>Gruppe:</b> "+ event.gruppenname + "</td><td> <b>Fahrer:</b> </td><td></td><td></td><td></td><td></td><td><td></td><td></td><td></td><td></td><td></td><td></td><td><i id='eventEntfernen' onclick='eventEntfernen("+event.id+")' class='fa-solid fa-xmark'></td> <tr><td colspan='10' id='beschreibung'>Beschreibung: "+ 
                 event.bemerkung + "</td></tr></table> </section>";
             });
             $("#uebersichtMain").html(inhalt);
