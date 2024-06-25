3465f83b413e7f35fa6ef13584873e54daf74470
FelixSauter <felixsauter100@gmail.com>
2024-06-25 16:01:57 +0200

    gute

--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -11,8 +11,8 @@ function dynamischEventUebersicht(){
             data.forEach(function(event){
                 console.log(event);
                 
-                        inhalt += "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b>"+ event.eventname+" </td> <td> <b>Ort:</b> " + event.ort + "</td> <td> <b>Zeit:</b> " + event.zeit 
-                        + "</td><td> <b>Gruppe:</b> "+ event.gruppenname + "</td><td> <b>Fahrer:</b> </td><td></td><td></td><td></td><td></td><td><td></td><td></td><td></td><td></td><td></td><td></td><td><i id='eventEntfernen' class='fa-solid fa-xmark'></td> <tr><td colspan='10' id='beschreibung'>Beschreibung: "+ 
+            inhalt += "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b>"+ event.eventname+" </td> <td> <b>Ort:</b> " + event.ort + "</td> <td> <b>Zeit:</b> " + event.zeit 
+                + "</td><td> <b>Gruppe:</b> "+ event.gruppenname + "</td><td> <b>Fahrer:</b> </td><td></td><td></td><td></td><td></td><td><td></td><td></td><td></td><td></td><td></td><td></td><td><i id='eventEntfernen' class='fa-solid fa-xmark'></td> <tr><td colspan='10' id='beschreibung'>Beschreibung: "+ 
                         event.bemerkung + "</td></tr></table> </section>";
                     })
             
@@ -20,8 +20,6 @@ function dynamischEventUebersicht(){
             //Fahrersuche: alle Namen in der Datenbank ausgegeben, die an dem Event teilnehmen (count(*) benutzen, aber wir ruft man 
             //die auf???)und dann in eine Liste packen und dann
             //einen Zufallszahlengenerator anwenden und die Person, die an dieser Stelle steht muss fahren
-            
-            $("#uebersichtMain").html(inhalt);
         },
         error: function(error){
             console.error("Error: ", error) 
