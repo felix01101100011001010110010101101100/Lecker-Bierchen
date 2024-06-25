7be07e68b9fcbd175e3549b27316f9370d24301c
pichen2 <pia.bauer.1@gmx.de>
2024-06-25 15:54:52 +0200

    Änderungen

--- public/scripts/event_uebersicht.js
+++ public/scripts/event_uebersicht.js
@@ -1,33 +1,29 @@
 function dynamischEventUebersicht(){
     // id aus dem Session Storage abrufen. Er wird bei der verification mit abgespeichert
     id = sessionStorage.getItem('id');
-    gruppenid = sessionStorage.getItem('gruppenid');
+    //gruppenid = sessionStorage.getItem('gruppenid');
     $.ajax({
-        url: "/eventUebersicht/"+id,
+        url: "/eventUebersicht/${id}",
         type: "GET",
         beforeSend: setAuthentification,
         success: function(data){
             //hier muss die Berechnung der Fahrersuche hin
             var inhalt = ""
+            console.log(data)
+            
             data.forEach(function(event){
-                console.log(event);
-                $.ajax({
-                    url: "/getGruppename",
-                    type: "GET",
-                    beforeSend: setAuthentification,
-                    data: {gruppenid: event.gruppenid},
-                    success: function(success){
-                        inhalt += "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b>"+ event.eventname+" </td> <td> <b>Ort:</b> " + event.ort + "</td> <td> <b>Zeit:</b> " + event.zeit 
-                        + "</td><td> <b>Gruppe:</b> "+ success.gruppenname + "</td><td> <b>Fahrer:</b> </td><td></td><td></td><td></td><td></td><td><td></td><td></td><td></td><td></td><td></td><td></td><td><i id='eventEntfernen' class='fa-solid fa-xmark'></td> <tr><td colspan='10' id='beschreibung'>Beschreibung: "+ 
-                        event.bemerkung + "</td></tr></table> </section>";
-                    }
-                })
-            })
+                inhalt += "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b> ${event.eventname} </td> <td> <b>Ort:</b> " + event.ort + "</td> <td> <b>Zeit:</b> " + event.zeit 
+                + "</td><td> <b>Gruppe:</b> "+ event.gruppenname + "</td><td> <b>Fahrer:</b> </td><td></td><td></td><td></td><td></td><td><td></td><td></td><td></td><td></td><td></td><td></td><td><i id='eventEntfernen' class='fa-solid fa-xmark'></td> <tr><td colspan='10' id='beschreibung'>Beschreibung: "+ 
+                event.bemerkung + "</td></tr></table> </section>";
+            });
+            $("#uebersichtMain").html(inhalt);
+             /*   
+            inhalt += "<section id='uebersichtSection'><table id='eventnameUebersicht'><tr><td> <b>Eventname:</b>"+ event.eventname+" </td> <td> <b>Ort:</b> " + event.ort + "</td> <td> <b>Zeit:</b> " + event.zeit 
+                + "</td><td> <b>Gruppe:</b> "+ event.gruppenname + "</td><td> <b>Fahrer:</b> </td><td></td><td></td><td></td><td></td><td><td></td><td></td><td></td><td></td><td></td><td></td><td><i id='eventEntfernen' class='fa-solid fa-xmark'></td> <tr><td colspan='10' id='beschreibung'>Beschreibung: "+ 
+                event.bemerkung + "</td></tr></table> </section>";*/
             //Fahrersuche: alle Namen in der Datenbank ausgegeben, die an dem Event teilnehmen (count(*) benutzen, aber wir ruft man 
             //die auf???)und dann in eine Liste packen und dann
             //einen Zufallszahlengenerator anwenden und die Person, die an dieser Stelle steht muss fahren
-            
-            $("#uebersichtMain").html(inhalt);
         },
         error: function(error){
             console.error("Error: ", error) 
