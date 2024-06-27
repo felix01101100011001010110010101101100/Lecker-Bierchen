3cc4ce5b68825e859d29abb9cb181339682745ab
pichen2 <pia.bauer.1@gmx.de>
2024-06-27 15:09:32 +0200

    fahrersuche event löschen

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -29,7 +29,7 @@ function dynamischEventInGruppe(gruppenid){
                 "<p id='beschreibung'>Beschreibung: "+ event.bemerkung+ "</p><p id='fahrername'> </p>"+
                 "<p><button type='submit' class='erstellen' id='dabei' value='1' onclick='eventDabei("+event.eventName+")'>Bin dabei!</button>"+
                 "<button type='submit' class='erstellen' id='remove' onclick='eventLoeschen("+event.eventid+")'> Löschen</button>"+
-                "<button type='submit' class='erstellen' id='fahrer' onclick='fahrerSuche(event.eventid)'>Fahrer suchen!</button></p> </section>";  
+                "<button type='submit' class='erstellen' id='fahrer' onclick='fahrerSuche("+event.eventid+")'>Fahrer suchen!</button></p> </section>";  
                 console.log(event);
                 $("#events").html(inhalt);
             })
