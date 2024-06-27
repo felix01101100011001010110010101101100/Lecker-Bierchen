c192a2b5d3642342b9c8e0f9b4fa329ae5d24d07
Kajuk5603 <kajuk5603@gmail.com>
2024-06-27 15:12:31 +0200

    window neu laden bei Mitglieder kicken

--- public/scripts/gruppen.js
+++ public/scripts/gruppen.js
@@ -154,6 +154,19 @@ function mitgliederKicken() {
             console.error("Error: ", error);
             alert("Sie konnten das Mitglied nicht kicken");
         }
+    }))
+    .then($.ajax({
+        url: "/gruppen.html",
+        type: "GET",
+        beforeSend: setAuthentification,
+        success: function(data){
+            $("body").html(data)
+            dynamischEventInGruppe(gruppenid)
+        },
+        error: function(error){
+            console.error("Error ", error);
+            alert("Seite konnte nicht geladen werden");
+        },
     }));
     
 }
